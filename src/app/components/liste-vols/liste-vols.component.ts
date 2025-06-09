5da5d2be846781c560e5a672bbecd0dd2905e1f0
Nath <nathan.girardi@proton.me>
2025-06-09 11:05:04 +0200

    modification affichage

--- src/app/components/liste-vols/liste-vols.component.ts
+++ src/app/components/liste-vols/liste-vols.component.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Vol } from '../../models/vol.model';
 import { CommonModule } from '@angular/common';
 import { VolComponent } from "../vol/vol.component";
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
     selector: 'app-liste-vols',
-    imports: [CommonModule, VolComponent],
+    imports: [CommonModule, VolComponent, MatIcon],
     templateUrl: './liste-vols.component.html',
 })
 export class ListeVolsComponent {
