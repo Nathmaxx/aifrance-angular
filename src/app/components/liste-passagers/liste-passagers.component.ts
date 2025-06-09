d0585e23417c1ac3a51867456bda4193738f6aed
Nath <nathan.girardi@proton.me>
2025-06-09 09:17:41 +0200

    appel api pour récupérer les passagers

--- src/app/components/liste-passagers/liste-passagers.component.ts
+++ src/app/components/liste-passagers/liste-passagers.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Vol } from '../../models/vol.model';
 import { VolComponent } from "../vol/vol.component";
 import { CommonModule } from '@angular/common';
+import { PassengerService } from '../../services/passengers.service';
+import { Passager } from '../../models/passager.model';
 
 @Component({
     selector: 'app-liste-passagers',
@@ -12,15 +14,30 @@ export class ListePassagersComponent implements OnChanges {
 
     @Input() flight!: Vol
     flightIsValid: boolean = false;
+    passengers!: Passager[]
+
+    constructor(private passengersService: PassengerService) { }
 
     ngOnChanges(changes: SimpleChanges) {
         if (changes['flight']) {
             this.flightIsValid = this.isNotNull();
-            console.log('Flight changed, isValid:', this.flightIsValid);
+            if (this.flightIsValid) {
+                this.passengersService.getPassengers(this.flight.icao)
+                    .subscribe({
+                        next: (passengers) => {
+                            this.passengers = passengers
+                        },
+                        error: (error) => {
+                            console.error('Erreur lors de la récupération des passagers :', error);
+                        }
+                    });
+            }
         }
     }
 
     isNotNull() {
         return !!this.flight && Object.keys(this.flight).length > 0;
     }
+
+
 }
--- src/app/services/passengers.service.ts
+++ src/app/services/passengers.service.ts
@@ -1,13 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IPassagerDto, Passager } from '../models/passager.model';
+import { map, Observable, tap } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class PassengerService {
 
-	constructor() { }
+	constructor(private http: HttpClient) { }
 
-	getPassengers() {
+	getPassengers(icao: string): Observable<Passager[]> {
+		const link = `https://randomuser.me/api/?results=20&inc=name,picture&seed=${icao}`
+		return this.http.get<any>(link).pipe(
+			map((response) => response.results
+				.map((dto: IPassagerDto) => new Passager(dto))
+			));
 
 	}
+
+	// https://randomuser.me/api/?results=20&inc=name,picture&seed=<numéro_icao>
+
 }
